dbe3234aebf85aa5dbd7eae9c625866a7dc2e564
hannanel100 <hannanel100@gmail.com>
2022-11-24 11:35:17 +0200

    added react-query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { CustomRoutes } from "./routes/routes";
 import Header from "./components/Header";
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Header />
       <CustomRoutes />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
